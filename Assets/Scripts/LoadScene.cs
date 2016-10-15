e7523b6c4402939bf0581ed4edc97f8044d25762
dmitrii <dvzyrianov@fullsail.edu>
2016-10-15 19:53:33 -0400

    Initial Commit

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+
+    //Variables
+    public GameObject loadingImage;
+
+
+
+    // takes the index of the scene from the build settings
+    public void LoadLevel(int _level)
+    {
+        loadingImage.SetActive(true);
+        SceneManager.LoadScene(_level);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+}
