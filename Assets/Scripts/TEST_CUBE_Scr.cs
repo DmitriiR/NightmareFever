5c316fb60cdd3d55e4b51c3bd9d1267b79f3b673
dmitrii <dvzyrianov@fullsail.edu>
2016-10-27 14:54:25 -0400

    Added some test assets and UI

---
+++ Assets/Scripts/TEST_CUBE_Scr.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TEST_CUBE_Scr : MonoBehaviour {
+
+    public int int1;
+
+    Material mat;
+
+    GameObject button2;
+    Button theButtonComponet;
+
+
+    void Start ()
+    {
+        // mat.color = Color.green;
+
+        mat = this.GetComponent<MeshRenderer>().material;
+        if (mat == true)
+            Debug.Log("Found the material just fine");
+
+
+        button2 = GameObject.FindGameObjectWithTag("Button2");
+        if (button2)
+            Debug.Log("button found");
+	}
+
+    public void ChangeToRed()
+    {
+        Debug.Log("Changing to Red");
+
+       // Color col = Vector3(10.0f,10.0f, 10.0f);
+
+        if (mat)
+            mat.color = Color.red;
+        if(button2)
+        button2.GetComponentInChildren<Text>().text = "READY";
+        theButtonComponet =  button2.GetComponent<Button>();
+        theButtonComponet.onClick.AddListener(ChangeMeFunc);
+    }
+
+
+    void ChangeMeFunc()
+    {
+
+        mat.color = Color.green;
+
+    }
+
+
+
+
+
+	
+	void Update ()
+    {
+
+      
+
+    }
+}
