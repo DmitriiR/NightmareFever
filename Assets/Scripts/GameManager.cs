7881e4d8d80236880b7dcba03b336970f32e7381
dmitrii <dvzyrianov@fullsail.edu>
2016-10-30 23:36:43 -0400

    made Timer

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,24 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 // RUNS THE ACTUAL GAME
 
 public class GameManager : MonoBehaviour {
 
     // Use this for initialization
+    float gameTime = 0.0f;
+    int gameTimeInt = 0;
+    GameObject timerTextRef;
+    
+
     public float LavaFlowSpeed;
     public float PlatformSpeed;
     public float LavaDamage;
 	void Start ()
     {
-	
-	}
+        gameTime = 0.0f;
+        timerTextRef = GameObject.FindGameObjectWithTag("TimerText");
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        // timer
+        gameTime += Time.deltaTime;
+        gameTimeInt = (int)gameTime;
+        timerTextRef.GetComponent<Text>().text = gameTimeInt.ToString();
+    }
 
     public void ReturnToMainMenu()
     {
--- Assets/Scripts/PlatformScr.cs
+++ Assets/Scripts/PlatformScr.cs
@@ -1,33 +1,53 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class PlatformScr : MonoBehaviour {
 
 
-    
-    float backSpeed = 5.0f; 
+    bool touching = false;
+    float backSpeed = 5.0f;
+ 
     // Use this for initialization
     void Start ()
     {
        backSpeed = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>().PlatformSpeed;
+      
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
+      // mvoe the platform
       Vector3 newPos = new Vector3(transform.position.x , transform.position.y , transform.position.z + backSpeed);
       transform.position = newPos;
+
+     
+
     }
 
     void OnTriggerEnter(Collider col)
     {
         if (col.gameObject.tag == "player")
         {
+
             GameObject tempPlayer = col.gameObject;
-
+            float pushUpDistance = this.transform.localScale.y ;
+            Vector3 newPos = new Vector3(tempPlayer.transform.position.x, tempPlayer.transform.position.y + pushUpDistance, tempPlayer.transform.position.z);
+            float time = 1.0f;
+            var i = 0.0f;
+            var rate = 1.0f / time;
+            while (i < 1.0f)
+            {
+                i += Time.deltaTime * rate;
+                tempPlayer.transform.position = Vector3.Lerp(tempPlayer.transform.position, newPos, i);
+            }
             this.GetComponent<Renderer>().material.color = Color.yellow;
-
         }
+    }
 
+    void OnCollisionEnter(Collision col)
+    {
+       
     }
 }
