3aef6914508372a9226c8eef0b0be5c2c73ebaec
dmitrii <dvzyrianov@fullsail.edu>
2016-11-08 18:22:11 -0500

    made imrovements to plsyer

--- Assets/Scripts/PlatformScr.cs
+++ Assets/Scripts/PlatformScr.cs
@@ -6,13 +6,15 @@ public class PlatformScr : MonoBehaviour {
 
 
     bool touching = false;
-    float backSpeed; 
- 
+    float backSpeed;
+
+    Light light;
     // Use this for initialization
     void Start ()
     {
        backSpeed = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>().PlatformSpeed;
-      
+        light = GetComponentInChildren<Light>();
+        light.enabled = false;
     }
 	
 	// Update is called once per frame
@@ -29,17 +31,22 @@ public class PlatformScr : MonoBehaviour {
         if (col.gameObject.tag == "player")
         {
             GameObject tempPlayer = col.gameObject;
-            //float pushUpDistance = this.transform.localScale.y ;
-   
-            
+
+
+            //tempPlayer.GetComponent<Rigidbody>().AddForce(new Vector3(0.0f, 0.0f, 1000.0f));
 
 
 
+            // make light turn on
+            light.enabled = true;
 
+
+
+            /*
             // force push up 
-            float pushUpDistance = 0.0f;
-
+            float pushUpDistance = 0.0f; // set to 0 for debigging
             Vector3 newPos = new Vector3(tempPlayer.transform.position.x, tempPlayer.transform.position.y + pushUpDistance, tempPlayer.transform.position.z);
+            Vector3 newPos = new Vector3(tempPlayer.transform.position.x, tempPlayer.transform.position.y,  transform.position.z);
             float time = 1.0f;
             var i = 0.0f;
             var rate = 1.0f / time;
@@ -48,8 +55,11 @@ public class PlatformScr : MonoBehaviour {
                 i += Time.deltaTime * rate;
                 tempPlayer.transform.position = Vector3.Lerp(tempPlayer.transform.position, newPos, i);
             }
-            this.GetComponent<Renderer>().material.color = Color.yellow;
-        }
+            */
+            
+    this.GetComponent<Renderer>().material.color = Color.yellow;
+        
+    }
     }
 
     void OnCollisionEnter(Collision col)
