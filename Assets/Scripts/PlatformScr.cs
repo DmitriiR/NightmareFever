8ffd754bf267a3a119040ee0a9f2ba4220f27754
dmitrii <dvzyrianov@fullsail.edu>
2016-11-16 15:51:24 -0500

    changed the order platform lights

--- Assets/Scripts/PlatformScr.cs
+++ Assets/Scripts/PlatformScr.cs
@@ -19,7 +19,8 @@ public class PlatformScr : MonoBehaviour {
     {
        backSpeed = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>().PlatformSpeed;
         light = GetComponentInChildren<Light>();
-        light.enabled = false;
+        light.enabled = true;
+        this.GetComponent<Renderer>().material.color = Color.yellow;
     }
 	
 	// Update is called once per frame
@@ -32,8 +33,6 @@ public class PlatformScr : MonoBehaviour {
         //if the player is on the platform
         GameObject tempPlayer = GameObject.FindGameObjectWithTag("player");
         if (touching)
-
-        //if (touching && tempPlayer.GetComponent<PlayerControllerScr>().moveDirection == Vector3.zero)
         {
             if (!tempPlayer) Debug.LogError("Platform Could not find player tag");
             float zOffset = tempPlayer.transform.position.z - transform.position.z;
@@ -53,7 +52,7 @@ public class PlatformScr : MonoBehaviour {
             GameObject tempPlayer = col.gameObject;
 
             float zOffset =   tempPlayer.transform.position.z - transform.position.z ;
-            Vector3 newPos = new Vector3(tempPlayer.transform.position.x, tempPlayer.transform.position.y , tempPlayer.transform.position.z + zOffset);
+            Vector3 newPos = new Vector3(tempPlayer.transform.position.x, tempPlayer.transform.position.y, tempPlayer.transform.position.z + zOffset);
             tempPlayer.transform.position = newPos;
             // global var to signify per frame move
             touching = true;
@@ -61,26 +60,32 @@ public class PlatformScr : MonoBehaviour {
             entryOffestZ = zOffset;
 
             // make light on platform turn on
-            light.enabled = true;
+            light.enabled = false;
 
 
-
-            /*
+/*
+            float platformPosition = transform.position.z + transform.localScale.z * 5;
+            if (tempPlayer.transform.position.y < platformPosition)
+            {
+                Debug.LogError("Push Needed");
+           
             // force push up 
-            float pushUpDistance = 0.0f; // set to 0 for debigging
-            Vector3 newPos = new Vector3(tempPlayer.transform.position.x, tempPlayer.transform.position.y + pushUpDistance, tempPlayer.transform.position.z);
-            Vector3 newPos = new Vector3(tempPlayer.transform.position.x, tempPlayer.transform.position.y,  transform.position.z);
+            float pushUpDistance = 0.5f; // set to 0 for debigging
+            Vector3 newPos2 = new Vector3(tempPlayer.transform.position.x, tempPlayer.transform.position.y + pushUpDistance, tempPlayer.transform.position.z);
+          //  Vector3 newPos = new Vector3(tempPlayer.transform.position.x, tempPlayer.transform.position.y,  transform.position.z);
             float time = 1.0f;
             var i = 0.0f;
             var rate = 1.0f / time;
             while (i < 1.0f)
             {
                 i += Time.deltaTime * rate;
-                tempPlayer.transform.position = Vector3.Lerp(tempPlayer.transform.position, newPos, i);
+                tempPlayer.transform.position = Vector3.Lerp(tempPlayer.transform.position, newPos2, i);
             }
-            */
+            }
+*/
             
-    this.GetComponent<Renderer>().material.color = Color.yellow;
+            
+    this.GetComponent<Renderer>().material.color = Color.gray;
         
     }
     }
